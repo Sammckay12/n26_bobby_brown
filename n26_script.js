bb1d29a747c3d3bb31b57c7fa91d2244315652fc
Sam Mckay <sammckay12@gmail.com>
2019-04-04 16:50:31 +0100

    node script not server

---
+++ n26_script.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const N26 = require('n26');
+
+function getTransactions() {
+  const myAccount = new N26('robholdenflag@gmail.com', '7v6GpL7ip8fjwUax2Q')
+  .then(account => account.transactions())
+  .then(transactions => {
+    writeToFile('./transactions.json', transactions)
+  });
+}
+
+ function writeToFile(path, data) {
+  console.log("data", data.length);
+    console.log("in write to file");
+    fs.writeFile(path, JSON.stringify(data, null,4), (err) => {
+      if (err) throw err;
+      console.log(path, 'saved!');
+    })
+  }
+
+  getTransactions()
